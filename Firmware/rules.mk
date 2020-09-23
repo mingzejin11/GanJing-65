20c385d18f191a9df72ea22689397d3d1cd312f2
Mingze Jin <mingze.jin11@gmail.com>
2020-09-23 10:42:37 -0400

    Added WPM calculation

--- Firmware/rules.mk
+++ Firmware/rules.mk
@@ -33,8 +33,8 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-ENCODER_ENABLE = yes
-OLED_DRIVER_ENABLE = yes
-
+ENCODER_ENABLE = yes		# Enable the encoder
+OLED_DRIVER_ENABLE = yes	# Enable the OLED
+WPM_ENABLE = yes			# Calculates the words per minute as a rolling average of your keystrokes and outputs as an unsigned integer
 
 LAYOUTS = LAYOUT_65_ansi LAYOUT_65_ansi_winkeyless LAYOUT_65_iso LAYOUT_65_iso_winkeyless
